c6ba36f0ee63a10969e6019c637c885558c2e33e
wyjw <yujian.wu1@gmail.com>
2020-07-08 14:30:43 +0800

    add experiments

--- uring-experiments/exp/exp1.sh
+++ uring-experiments/exp/exp1.sh
@@ -1,1 +1,1 @@
-fio ../open-loop/run.fio -filename /dev/nvme0n1 --ioengine=io_uring --rate_iops=100000 --timeout 30 --output a.test
+fio ../open-loop/run.fio -filename /dev/nvme0n1 --ioengine=io_uring --rate_iops=40000 --timeout 30 --output cn.test
